cfb2e32fc2afcbfa7549b0b843102c6aef462395
Argore <argoreyt@gmail.com>
2025-03-20 16:55:14 +0100

    unifrm ideas

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 std::string generateBasicVertShader();
 std::string generateBasicFragShader();
+std::string generateVulkanFragShader();
 
 int main()
 {
@@ -15,6 +16,9 @@ int main()
 
 	std::ofstream fs( "generated/basic_fs.glsl" );
 	fs << generateBasicFragShader();
+
+	std::ofstream fs( "generated/vk_fs.glsl" );
+	fs << generateVulkanFragShader();
 
 	return 0;
 }
@@ -82,6 +86,70 @@ std::string generateBasicFragShader()
 	factory.setOutputValue( "out_Albedo", "color" );
 	factory.setOutputValue( "out_Normal", "normal" );
 	factory.setOutputValue( "out_RoughnessMetallic", "#{1.0}" );
+	// }
+
+	return factory.build();
+}
+
+std::string generateVulkanFragShader()
+{
+	wv::GLSLFactory factory;
+	factory.setStage( wv::Shader::kFragment );
+	factory.addInput( "int",    "inDrawID",      true,  0 );
+	factory.addInput( "vec4",   "in_Normal",     false, 1 );
+	factory.addInput( "vec2",   "in_Position",   false, 2 );
+	factory.addInput( "vec2 ",  "in_InstanceID", false, 3 );
+	factory.addInput( "vec3",   "in_HasAlpha",   false, 4 );
+	factory.addInput( "mat3x3", "in_Albedo",     false, 5 );
+	factory.addInput( "int",    "in_Albedo",     true,  6 );
+
+	factory.addOutput( "vec4", "outFragColor", false, 0 );
+
+	// uniforms
+
+	/*
+
+	factory.beginUniformStruct( "u_Material", 0, 1, 255 );
+	factory.addUniform( "vec4", "MaterialColor" );
+	factory.addUniform( "vec4", "LightColor" );
+	factory.addUniform( "bool", "IsVideo" );
+	factory.addUniform( "bool", "IsCubemapped" );
+	factory.endUniformStruct();
+
+	factory.addUniform( 0, 2, 255, "texture", "u_Colormap"      );
+	factory.addUniform( 0, 3, 255, "texture", "u_ColormapY"     );
+	factory.addUniform( 0, 4, 255, "texture", "u_ColormapUV"    );
+	factory.addUniform( 0, 5, 255, "texture", "u_Normalmap"     );
+	factory.addUniform( 0, 6, 255, "texture", "u_Specularmap"   );
+	factory.addUniform( 0, 7, 255, "texture", "u_Lightmap"      );
+	factory.addUniform( 0, 8, 255, "texture", "u_Emissivemap"   );
+	factory.addUniform( 0, 9, 255, "cubemap", "u_Reflectionmap" );
+
+
+
+	becomes:
+
+
+
+	layout(set = 0, binding = 1) uniform MaterialProperties {
+		vec4 MaterialColor;
+		vec4 LightColor;
+		bool IsVideo;
+		bool IsCubemapped;
+	} u_Material[];
+
+	layout(set = 0, binding = 2) uniform sampler2D u_Colormap[];
+	layout(set = 0, binding = 3) uniform sampler2D u_ColormapY[];
+	layout(set = 0, binding = 4) uniform sampler2D u_ColormapUV[];
+	layout(set = 0, binding = 5) uniform sampler2D u_Normalmap[];
+	layout(set = 0, binding = 6) uniform sampler2D u_Specularmap[];
+	layout(set = 0, binding = 7) uniform sampler2D u_Lightmap[];
+	layout(set = 0, binding = 8) uniform sampler2D u_Emissivemap[];
+	layout(set = 0, binding = 9) uniform samplerCube u_Reflectionmap[];
+	*/
+
+	// void main() {
+	factory.setOutputValue( "outFragColor", "#{1.0}" );
 	// }
 
 	return factory.build();
